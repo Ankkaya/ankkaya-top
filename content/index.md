94cf701a9b8089930ac90db4b36ef3d91b473cdc
Ankkaya <staryou@126.com>
2023-10-24 10:47:02 +0800

    完成首页

---
+++ content/index.md
@@ -0,0 +1,12 @@
+---
+name: Ankkaya
+caption: Hey there, I'm Ankkaya
+description: I'm a frontend developer, using vue, vite, nuxt, tailwindcss, vant
+image: /assets/img/ankkaya.jpg
+socials:
+  [
+    { name: "Twitter", url: "https://twitter.com/ankkayaaa", text: "TW" },
+    { name: "Tiktok", url: "https://www.tiktok.com/@ankkayaa", text: "TI" },
+    { name: "Github", url: "https://github.com/Ankkaya", text: "GI" },
+  ]
+---
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  content: {
+    highlight: {
+      theme: {
+        default: 'material-theme-palenight',
+        dark: 'github-dark',
+      },
+    },
+    markdown: {
+      toc: {
+        depth: 5,
+        searchDepth: 5,
+      },
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
 })
